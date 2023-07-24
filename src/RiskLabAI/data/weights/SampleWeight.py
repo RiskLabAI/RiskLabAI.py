edaa9111f942e1dbb11c38be8a7da0fef64f59c8
Ahmad Aghapour <107943830+ahmad-aghapour@users.noreply.github.com>
2023-07-24 14:36:46 -0400

    book code additions

---
+++ src/RiskLabAI/data/weights/SampleWeight.py
@@ -0,0 +1,120 @@
+
+import imp
+import numpy as np 
+import pandas as pd 
+
+"""
+function: expand label tO incorporate meta-labeling
+reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
+methodology: SNIPPET 4.1
+"""
+def concurrencyEvents(closeIndex, # DataFrame that has events
+                      timestamp, # DateFrame that has return and label of each period
+                      molecule): # index that function must apply on it
+    #1) find events that span the period [molecule[0],molecule[-1]]
+    timestamp=timestamp.fillna(closeIndex[-1]) # unclosed events still must impact other weights
+    timestamp=timestamp[timestamp>=molecule[0]] # events that end at or after molecule[0]
+    timestamp=timestamp.loc[:timestamp[molecule].max()] # events that start at or before t1[molecule].max()
+    #2) count events spanning a bar
+    iloc=closeIndex.searchsorted(np.array([timestamp.index[0],timestamp.max()]))
+    count=pd.Series(0,index=closeIndex[iloc[0]:iloc[1]+1])
+    for tIn,tOut in timestamp.iteritems():
+        count.loc[tIn:tOut]+=1. #add for new events 
+
+    return count.loc[molecule[0]:timestamp[molecule].max()]
+
+"""
+function: SampleWeight with triple barrier
+reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
+methodology: SNIPPET 4.2
+"""
+def mpSampleWeight(timestamp, # DataFrame of events start and end for labelling 
+                   concurrencyEvents, # Data frame of concurrent events for each events 
+                   molecule) :# index that function must apply on it
+    # Derive average uniqueness over the event's lifespan
+    weight=pd.Series(index=molecule) # create pandas object fot weights 
+    for tIn,tOut in timestamp.loc[weight.index].iteritems():
+        weight.loc[tIn]=(1./concurrencyEvents.loc[tIn:tOut]).mean() #compute sample weight according to book equation 
+    return weight
+
+"""
+function: Creating Index matrix 
+reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
+methodology: SNIPPET 4.3 
+"""
+def index_matrix(barIndex, #index of all data 
+                timestamp): #times of events contain starting and ending time 
+    # Get indicator matrix
+    indM=pd.DataFrame(0,index=barIndex,columns=range(timestamp.shape[0]))
+    for row in timestamp.itertuples():
+        t0 =  int(row.date) 
+        t1 = int(row.timestamp)
+        indM.loc[t0:t1,row.Index]=1.
+    return indM
+
+"""
+function: compute average uniqueness
+reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
+methodology: SNIPPET 4.4
+"""   
+def averageUniqueness(indexMatrix):
+    # Average uniqueness from indicator matrix
+    c=indexMatrix.sum(axis=1) # concurrency
+    u=indexMatrix.div(c,axis=0) # uniqueness
+    averageUniqueness_=u[u>0].mean() # average uniqueness
+    return averageUniqueness_
+
+"""
+function:  SequentialBootstrap implementation 
+reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
+methodology: SNIPPET 4.5
+"""
+def sequential_bootstrap(indexMatrix, #matrix that Indicator for events 
+                        sampleLength): # number of sample :
+    # Generate a sample via sequential bootstrap
+    if sampleLength is None:sampleLength=indexMatrix.shape[1]
+    phi=[]
+    while len(phi)<sampleLength:
+        averageUniqueness_=pd.Series(dtype=np.float64)
+        for i in indexMatrix:
+            indexMatrix_=indexMatrix[phi+[i]] # reduce indM
+            averageUniqueness_.loc[i]=averageUniqueness(indexMatrix_).iloc[-1]
+        prob=averageUniqueness_/averageUniqueness_.sum() # draw prob
+        phi+=[np.random.choice(indexMatrix.columns,p=prob)]
+    return phi
+
+"""
+    function:  sample weight with returns 
+    reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
+    methodology: SNIPPET 4.10
+"""
+def mpSampleWeightAbsoluteReturn(timestamp, # dataFrame for events 
+                                 concurrencyEvents,# dataframe that contain number of concurrent events for each events
+                                 returns,#data frame that contains returns
+                                 molecule): # molecule
+# Derive sample weight by return attribution
+    return_=np.log(returns).diff() # log-returns, so that they are additive
+    weight=pd.Series(index=molecule)
+    for tIn,tOut in timestamp.loc[weight.index].iteritems():
+        weight.loc[tIn]=(return_.loc[tIn:tOut]/concurrencyEvents.loc[tIn:tOut]).sum() # compute sample weight 
+    return weight.abs()
+
+"""
+    function:  compute TimeDecay
+    reference: De Prado, M. (2018) Advances in financial machine learning. John Wiley & Sons.
+    methodology: SNIPPET 4.11
+"""
+def timeDecay(weight, #weight that compute for each event 
+              clfLastW = 1.0): #weight of oldest observation :
+    # apply piecewise-linear decay to observed uniqueness (weight)
+    # newest observation gets weight=1, oldest observation gets weight=clfLastW
+    clfW=weight.sort_index().cumsum() # compute cumulative sum of weights 
+    if clfLastW>=0:
+        slope=(1.-clfLastW)/clfW.iloc[-1] # compute slope of line 
+    else:
+        slope=1./((clfLastW+1)*clfW.iloc[-1]) # compute slope of line 
+    const=1.-slope*clfW.iloc[-1] # compute b in y =ax + b 
+    clfW=const+slope*clfW # compute points on line 
+    clfW[clfW<0]=0 # if clfw is less than zero set that entry to zero 
+    return clfW
+
---
+++ src/RiskLabAI/modeling/HyperParamterTuning.py
@@ -0,0 +1,46 @@
+from Python_Chapter_7 import *
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import BaggingClassifier
+from sklearn.pipeline import Pipeline
+class MyPipeline(Pipeline):
+    def fit(self,X,y,sample_weight=None,**fit_params):
+        if sample_weight is not None:
+            fit_params[self.steps[-1][0]+'__sample_weight']=sample_weight
+        return super(MyPipeline,self).fit(X,y,**fit_params)
+
+
+"""
+function: hyper parameter tuning 
+reference: De Prado, M. (2018) Advances in financial machine learning.
+methodology: page 131, snippet 9.3
+"""
+
+def clfHyperFit(feature_data, # data of features 
+                label, # labels of data 
+                time, # observation time
+                pipe_clf, # our estimator
+                param_grid, # parameter space 
+                cv=3, # number of group for cross validation 
+                bagging=[0,-1,1.], # bagging type
+                rndSearchIter=0,
+                n_jobs=-1,
+                percent_embargo=0, # percent of embergo
+                **fit_params):
+    if set(label.values)=={0,1}:
+        scoring='f1' # f1 for meta-labeling
+    else:
+        scoring='neg_log_loss' # symmetric towards all cases
+    #1) hyperparameter search, on train data
+    inner_cv=PurgedKFold(n_splits=cv,times=time,percent_embargo=percent_embargo) # purged
+    if rndSearchIter==0:
+        gs=GridSearchCV(estimator=pipe_clf,param_grid=param_grid,scoring=scoring,cv=inner_cv,n_jobs=n_jobs)
+    else:
+        gs=RandomizedSearchCV(estimator=pipe_clf,param_distributions= param_grid,scoring=scoring,cv=inner_cv,n_jobs=n_jobs,n_iter=rndSearchIter)
+    gs=gs.fit(feature_data,label,**fit_params).best_estimator_ # pipeline
+    #2) fit validated model on the entirety of the data
+    if bagging[1]>0:
+        gs=BaggingClassifier(base_estimator=MyPipeline(gs.steps), n_estimators=int(bagging[0]),max_samples=float(bagging[1]),max_features=float(bagging[2]),n_jobs=n_jobs)
+        gs=gs.fit(feature_data,label,sample_weight=fit_params[gs.base_estimator.steps[-1][0]+'__sample_weight'])
+        gs=Pipeline([('bag',gs)])
+    return gs
