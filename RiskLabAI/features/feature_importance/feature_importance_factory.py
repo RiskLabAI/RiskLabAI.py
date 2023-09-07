39ae07b9a4d426d8ef617c339ce128e6b8de90d8
Seyed Alireza Mousavizade <107984049+salireza-mousavizade@users.noreply.github.com>
2023-09-07 12:20:47 +0330

    add docs

--- RiskLabAI/features/feature_importance/feature_importance_factory.py
+++ RiskLabAI/features/feature_importance/feature_importance_factory.py
@@ -1,25 +1,47 @@
-# feature_importance_factory.py
-
-from feature_importance_strategy import (
-    FeatureImportanceStrategy,
-)  # Assuming the abstract class is in the same directory
-from sklearn.ensemble import RandomForestClassifier
-from typing import Dict, List, Union, Optional
+from feature_importance_strategy import FeatureImportanceStrategy
+from typing import Optional
 import pandas as pd
 
 
 class FeatureImportanceFactory:
-    def __init__(self) -> FeatureImportanceStrategy:
-        pass
+    """
+    Factory class for building and fetching feature importance computation results.
+
+    Usage:
+
+    .. code-block:: python
+
+        factory = FeatureImportanceFactory()
+        factory.build(my_feature_importance_strategy_instance)
+        results = factory.get_results()
+
+    """
+    def __init__(self) -> None:
+        """Initialize the FeatureImportanceFactory class."""
+        self._results: Optional[pd.DataFrame] = None
 
     def build(
-        self,
-        feature_importance_strategy: FeatureImportanceStrategy,
-    ) -> FeatureImportanceStrategy:
-        self.results = feature_importance_strategy.compute()
+        self, 
+        feature_importance_strategy: FeatureImportanceStrategy
+    ) -> 'FeatureImportanceFactory':
+        """
+        Build the feature importance based on the provided strategy.
+
+        :param feature_importance_strategy: An instance of a strategy 
+            inheriting from FeatureImportanceStrategy.
+        
+        :return: Current instance of the FeatureImportanceFactory.
+        """
+        self._results = feature_importance_strategy.compute()
         return self
 
-    def get(
-        self,
-    ) -> pd.DataFrame:
-        return self.results
+    def get_results(self) -> pd.DataFrame:
+        """
+        Fetch the computed feature importance results.
+
+        :return: Dataframe containing the feature importance results.
+        """
+        if self._results is None:
+            raise ValueError("Feature importance not yet computed.")
+        
+        return self._results
--- RiskLabAI/features/feature_importance/feature_importance_strategy.py
+++ RiskLabAI/features/feature_importance/feature_importance_strategy.py
@@ -1,10 +1,30 @@
 from abc import ABC, abstractmethod
 import pandas as pd
-import numpy as np
-from typing import Dict, List
 
 
 class FeatureImportanceStrategy(ABC):
+    """
+    Abstract Base Class for computing feature importance.
+
+    Derived classes must implement the `compute` method to 
+    provide their own logic for computing feature importance.
+    """
+
     @abstractmethod
-    def compute(self, *args, **kwargs) -> pd.DataFrame:
+    def compute(
+            self,
+            *args,
+            **kwargs
+    ) -> pd.DataFrame:
+        """
+        Abstract method to compute feature importance.
+
+        :param args: Positional arguments.
+        :param kwargs: Keyword arguments.
+        :return: A pandas DataFrame containing feature importances.
+
+        Note: Derived classes should provide a concrete implementation 
+        of this method with specific parameters and docstrings relevant 
+        to their implementation.
+        """
         pass
