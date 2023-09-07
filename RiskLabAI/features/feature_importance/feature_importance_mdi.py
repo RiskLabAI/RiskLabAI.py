75384012ae20e651325dc6f68a2c4c33931f6810
Seyed Alireza Mousavizade <107984049+salireza-mousavizade@users.noreply.github.com>
2023-09-07 12:06:36 +0330

    Feature Importance Re-architeched

--- RiskLabAI/features/feature_importance/feature_importance_mdi.py
+++ RiskLabAI/features/feature_importance/feature_importance_mdi.py
@@ -1,23 +1,29 @@
-from feature_importance_strategy import FeatureImportanceStrategy  # Assuming the abstract class is in the same directory
+from feature_importance_strategy import (
+    FeatureImportanceStrategy,
+)  # Assuming the abstract class is in the same directory
 import pandas as pd
 import numpy as np
 
+
 class FeatureImportanceMDI(FeatureImportanceStrategy):
-    def __init__(self, classifier: object, feature_names: list):
+    def __init__(self, classifier: object, x, y):
         self.classifier = classifier
-        self.feature_names = feature_names
+        classifier.fit(x, y)
 
     def compute(self) -> pd.DataFrame:
         feature_importances_dict = {i: tree.feature_importances_ for i, tree in enumerate(self.classifier.estimators_)}
-        feature_importances_df = pd.DataFrame.from_dict(feature_importances_dict, orient='index')
-        feature_importances_df.columns = self.feature_names
+        feature_importances_df = pd.DataFrame.from_dict(feature_importances_dict, orient="index")
+        feature_importances_df.columns = self.classifier.feature_names_in_
 
         feature_importances_df = feature_importances_df.replace(0, np.nan)  # because max_features=1
 
-        importances = pd.concat({
-            "Mean": feature_importances_df.mean(),
-            "StandardDeviation": feature_importances_df.std() * feature_importances_df.shape[0]**-0.5
-        }, axis=1)  # CLT
+        importances = pd.concat(
+            {
+                "Mean": feature_importances_df.mean(),
+                "StandardDeviation": feature_importances_df.std() * feature_importances_df.shape[0] ** -0.5,
+            },
+            axis=1,
+        )  # CLT
 
         importances /= importances["Mean"].sum()
 
