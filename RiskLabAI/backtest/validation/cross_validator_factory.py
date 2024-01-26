9d7bbb2d11398899e331e237406e99eecac33693
Daniel Norouzi M <108014662+danial-norouzi@users.noreply.github.com>
2024-01-26 21:44:20 +0330

    Update cross_validator_factory.py

--- RiskLabAI/backtest/validation/cross_validator_factory.py
+++ RiskLabAI/backtest/validation/cross_validator_factory.py
@@ -1,6 +1,5 @@
-from typing import Type
-import pandas as pd
 from .kfold import KFold
+from .walk_forward import WalkForward
 from .purged_kfold import PurgedKFold
 from .combinatorial_purged import CombinatorialPurged
 from .cross_validator_interface import CrossValidator
@@ -16,11 +15,11 @@ class CrossValidatorFactory:
             **kwargs
     ) -> CrossValidator:
         """
-        Factory method to create and return an instance of a cross-validator 
+        Factory method to create and return an instance of a cross-validator
         based on the provided type.
 
-        :param validator_type: Type of cross-validator to create. Options include 
-            'kfold', 'purgedkfold', and 'combinatorial_purged'.
+        :param validator_type: Type of cross-validator to create. Options include
+            'kfold', 'walkforward', 'purgedkfold', and 'combinatorialpurged'.
         :type validator_type: str
 
         :param kwargs: Additional keyword arguments to be passed to the cross-validator's constructor.
@@ -33,10 +32,13 @@ class CrossValidatorFactory:
         """
         if validator_type == 'kfold':
             return KFold(**kwargs)
-        # Add other cross-validators here
+            
+        elif validator_type == 'walkforward':
+            return WalkForward(**kwargs)
+
         elif validator_type == 'purgedkfold':
             return PurgedKFold(**kwargs)
-        
+
         elif validator_type == 'combinatorialpurged':
             return CombinatorialPurged(**kwargs)
 
