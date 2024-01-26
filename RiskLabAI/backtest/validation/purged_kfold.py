2813a11ae6c60cca7ffe93bd7eeb9a2b4be11bd0
Daniel Norouzi M <108014662+danial-norouzi@users.noreply.github.com>
2024-01-26 21:39:08 +0330

    Update purged_kfold.py

--- RiskLabAI/backtest/validation/purged_kfold.py
+++ RiskLabAI/backtest/validation/purged_kfold.py
@@ -3,6 +3,8 @@ from typing import Generator, Optional, Tuple, Union, Dict, List, Any, Set
 from joblib import Parallel, delayed
 import pandas as pd
 import numpy as np
+import warnings
+from sklearn.exceptions import ConvergenceWarning
 
 from .cross_validator_interface import CrossValidator
 
@@ -49,16 +51,10 @@ class PurgedKFold(CrossValidator):
         if embargo_length == 0:
             embargoed_data_info_range = pd.Series(test_time_range.values, index=test_time_range.values)
 
-        else:    
-            test_data_info_range = data_info_range[effective_test_time_range.index.min():]
-            embargoed_data_info_range = pd.Series(
-                test_data_info_range.iloc[embargo_length:],
-                index=test_data_info_range.iloc[:-embargo_length]
-            )
-            embargoed_data_info_range = pd.concat([
-                embargoed_data_info_range,
-                pd.Series(test_data_info_range[-1], index=test_data_info_range[-embargo_length:])
-            ])
+        else:
+            effective_sample = data_info_range.loc[test_time_range.index.min():].copy().drop_duplicates()
+            embargoed_data_info_range = pd.Series(effective_sample.values, index=effective_sample.values)
+            embargoed_data_info_range = embargoed_data_info_range.shift(-embargo_length).fillna(embargoed_data_info_range.values[-1]) 
 
         for start_ix, end_ix in effective_test_time_range.items():
             # Adding the embargo period
@@ -80,7 +76,7 @@ class PurgedKFold(CrossValidator):
             indices_to_drop.update(overlapping_end)
             indices_to_drop.update(enveloping)
 
-        return data_info_range.drop(indices_to_drop)   
+        return data_info_range.drop(indices_to_drop)
 
 
     def __init__(
@@ -92,9 +88,9 @@ class PurgedKFold(CrossValidator):
         """
         Purged k-fold cross-validation to prevent information leakage.
 
-        Implements a cross-validation strategy where each fold is purged 
-        of observations overlapping with the training set in the time dimension. 
-        An embargo period is also introduced to further prevent potential 
+        Implements a cross-validation strategy where each fold is purged
+        of observations overlapping with the training set in the time dimension.
+        An embargo period is also introduced to further prevent potential
         information leakage.
 
         Attributes:
@@ -111,7 +107,7 @@ class PurgedKFold(CrossValidator):
             - *times.value*: Time when the information extraction ended.
         :type times: pd.Series
 
-        :param embargo: The embargo period to further prevent potential 
+        :param embargo: The embargo period to further prevent potential
                         information leakage.
         :type embargo: float
         """
@@ -146,14 +142,14 @@ class PurgedKFold(CrossValidator):
 
 
     def _validate_input(
-        self, 
-        single_times: pd.Series, 
+        self,
+        single_times: pd.Series,
         single_data: pd.DataFrame
     ) -> None:
         """
         Validate that the input data and times share the same index.
 
-        This function checks if the provided data and its corresponding times 
+        This function checks if the provided data and its corresponding times
         have the same index. If they do not match, it raises a `ValueError`.
 
         :param single_times: Time series data to be validated.
@@ -168,15 +164,15 @@ class PurgedKFold(CrossValidator):
 
 
     def _get_train_indices(
-        self, 
-        test_indices: np.ndarray, 
+        self,
+        test_indices: np.ndarray,
         single_times: pd.Series,
         continous_test_times: bool = False,
     ) -> np.ndarray:
         """
         Obtain the training indices considering purging and embargo.
 
-        This function retrieves the training set indices based on the given test indices 
+        This function retrieves the training set indices based on the given test indices
         while considering the purging and embargo strategy.
 
         :param test_indices: Indices used for the test set.
@@ -225,13 +221,13 @@ class PurgedKFold(CrossValidator):
         labels: Optional[Union[pd.Series, Dict[str, pd.Series]]] = None,
         groups: Optional[np.ndarray] = None
     ) -> Union[
-        Generator[Tuple[np.ndarray, np.ndarray], None, None], 
+        Generator[Tuple[np.ndarray, np.ndarray], None, None],
         Generator[Tuple[str, Tuple[np.ndarray, np.ndarray]], None, None]
     ]:
         """
         Split multiple datasets into train and test sets.
 
-        This function either splits a single dataset or multiple datasets considering 
+        This function either splits a single dataset or multiple datasets considering
         purging and embargo.
 
         :param data: Dataset or dictionary of datasets.
@@ -241,7 +237,7 @@ class PurgedKFold(CrossValidator):
         :param groups: Group information, if available.
         :type groups: Optional[np.ndarray]
         :return: Train and test indices or key with train and test indices for multiple datasets.
-        :rtype: Union[Generator[Tuple[np.ndarray, np.ndarray], None, None], 
+        :rtype: Union[Generator[Tuple[np.ndarray, np.ndarray], None, None],
                     Generator[Tuple[str, Tuple[np.ndarray, np.ndarray]], None, None]]
         """
         if self.is_multiple_datasets:
@@ -254,19 +250,19 @@ class PurgedKFold(CrossValidator):
             for train_indices, test_indices in self._single_split(
                 self.times, data
             ):
-                yield train_indices, test_indices        
+                yield train_indices, test_indices
 
 
     def _single_backtest_paths(
-        self, 
-        single_times: pd.Series, 
-        single_data: pd.DataFrame, 
+        self,
+        single_times: pd.Series,
+        single_data: pd.DataFrame,
     ) -> Dict[str, List[Dict[str, np.ndarray]]]:
         """
         Generate backtest paths based on training and testing indices.
 
-        This function first validates the input data and times. Then, it generates 
-        the training and testing indices for backtesting. These paths are organized 
+        This function first validates the input data and times. Then, it generates
+        the training and testing indices for backtesting. These paths are organized
         into a dictionary with a designated name for each backtest path.
 
         :param single_times: Time series data for validation.
@@ -290,26 +286,29 @@ class PurgedKFold(CrossValidator):
         paths['Path 1'] = path_data
 
         return paths
-    
+
 
     def backtest_paths(
-        self, 
-        data: Union[pd.DataFrame, Dict[str, pd.DataFrame]], 
+        self,
+        data: Union[pd.DataFrame, Dict[str, pd.DataFrame]],
     ) -> Union[
-        Dict[str, List[Dict[str, np.ndarray]]], 
+        Dict[str, List[Dict[str, np.ndarray]]],
         Dict[str, Dict[str, List[Dict[str, List[np.ndarray]]]]]
     ]:
         """
         Generate backtest paths for single or multiple datasets.
 
-        This function checks whether multiple datasets are being used. If so, it iterates through each 
-        dataset, generating backtest paths using the `_single_backtest_paths` method. Otherwise, it directly 
+        This function checks whether multiple datasets are being used. If so, it iterates through each
+        dataset, generating backtest paths using the `_single_backtest_paths` method. Otherwise, it directly
         returns the backtest paths for the single dataset.
 
-        :param data: Dataset or dictionary of datasets.
+        :param data: Input data on which the backtest paths are based.
+                    Can be either a single DataFrame or a dictionary of DataFrames for multiple datasets.
         :type data: Union[pd.DataFrame, Dict[str, pd.DataFrame]]
-        :return: Backtest paths structured in a dictionary. For multiple datasets, an outer dictionary is used with 
-                dataset names as keys and individual backtest path dictionaries as values.
+
+        :return: A dictionary where each key is a backtest path name, and the value is
+                a list of dictionaries with train and test index arrays. For multiple datasets,
+                a nested dictionary structure is returned.
         :rtype: Union[
             Dict[str, List[Dict[str, np.ndarray]]],
             Dict[str, Dict[str, List[Dict[str, List[np.ndarray]]]]]
@@ -320,12 +319,12 @@ class PurgedKFold(CrossValidator):
             multiple_paths = {}
             for key in self.times:
                 multiple_paths[key] = self._single_backtest_paths(self.times[key], data[key])
-                
+
             return multiple_paths
         else:
-            
+
             return self._single_backtest_paths(self.times, data)
-        
+
 
     def _single_backtest_predictions(
         self,
@@ -340,8 +339,8 @@ class PurgedKFold(CrossValidator):
         """
         Obtain predictions for a single dataset during backtesting.
 
-        This function leverages parallel computation to train and predict on different train-test splits 
-        of a single dataset using a given estimator. It utilizes the `_single_split` method to generate 
+        This function leverages parallel computation to train and predict on different train-test splits
+        of a single dataset using a given estimator. It utilizes the `_single_split` method to generate
         the train-test splits.
 
         :param single_estimator: Estimator or model to be trained and used for predictions.
@@ -352,10 +351,10 @@ class PurgedKFold(CrossValidator):
         :type single_data: pd.DataFrame
         :param single_labels: Labels corresponding to the single dataset.
         :type single_labels: pd.Series
-        :param single_weights: Weights for the observations in the single dataset. 
+        :param single_weights: Weights for the observations in the single dataset.
                             Defaults to equally weighted if not provided.
         :type single_weights: np.ndarray, optional
-        :param predict_probability: If True, predict the probability of forecasts. 
+        :param predict_probability: If True, predict the probability of forecasts.
         :type predict_probability: bool
         :param n_jobs: The number of jobs to run in parallel. Default is 1.
         :type n_jobs: int, optional
@@ -376,25 +375,31 @@ class PurgedKFold(CrossValidator):
             y_train = single_labels.iloc[train_indices]
             weights_train = single_weights[train_indices]
 
-            single_estimator_.fit(X_train, y_train, weights_train)
-        
+            with warnings.catch_warnings():
+                warnings.filterwarnings('ignore', category=ConvergenceWarning)
+                try:
+                    single_estimator_.fit(X_train, y_train, sample_weight=weights_train)
+
+                except TypeError:
+                    single_estimator_.fit(X_train, y_train)
+
             X_test = single_data.iloc[test_indices]
 
             if predict_probability:
                 return single_estimator_.predict_proba(X_test)
-            
+
             else:
                 return single_estimator_.predict(X_test)
-                
+
         path_data = Parallel(n_jobs=n_jobs)(
             delayed(train_test_single_estimator)(
                 deepcopy(single_estimator), train_indices, test_indices
             ) for train_indices, test_indices in self._single_split(single_times, single_data)
         )
-        
+
         paths_predictions = {'Path 1': np.concatenate(path_data)}
 
-        return paths_predictions  
+        return paths_predictions
 
 
     def backtest_predictions(
@@ -409,23 +414,23 @@ class PurgedKFold(CrossValidator):
         """
         Generate backtest predictions for single or multiple datasets.
 
-        For each dataset, this function leverages the `_single_backtest_predictions` method to obtain 
+        For each dataset, this function leverages the `_single_backtest_predictions` method to obtain
         predictions for different train-test splits using the given estimator.
 
-        :param estimator: Model or estimator to be trained and used for predictions. 
+        :param estimator: Model or estimator to be trained and used for predictions.
                         Can be a single estimator or a dictionary of estimators for multiple datasets.
         :type estimator: Union[Any, Dict[str, Any]]
-        :param data: Input data for training and testing. Can be a single dataset or 
+        :param data: Input data for training and testing. Can be a single dataset or
                     a dictionary of datasets for multiple datasets.
         :type data: Union[pd.DataFrame, Dict[str, pd.DataFrame]]
-        :param labels: Target labels for training and testing. Can be a single series or 
+        :param labels: Target labels for training and testing. Can be a single series or
                     a dictionary of series for multiple datasets.
         :type labels: Union[pd.Series, Dict[str, pd.Series]]
-        :param sample_weights: Weights for the observations in the dataset(s). 
+        :param sample_weights: Weights for the observations in the dataset(s).
                             Can be a single array or a dictionary of arrays for multiple datasets.
                             Defaults to None, which means equal weights for all observations.
         :type sample_weights: Optional[Union[np.ndarray, Dict[str, np.ndarray]]]
-        :param predict_probability: If True, predict the probability of forecasts. 
+        :param predict_probability: If True, predict the probability of forecasts.
         :type predict_probability: bool
         :param n_jobs: The number of jobs to run in parallel. Default is 1.
         :type n_jobs: int, optional
@@ -437,7 +442,7 @@ class PurgedKFold(CrossValidator):
             multiple_paths_predictions = {}
             for key in self.times:
                 multiple_paths_predictions[key] = self._single_backtest_predictions(
-                    estimator[key], self.times[key], data[key], labels[key], 
+                    estimator[key], self.times[key], data[key], labels[key],
                     sample_weights[key] if sample_weights else None, predict_probability, n_jobs
                 )
             return multiple_paths_predictions
