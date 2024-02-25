68faae7e72761ce25f1981ed3f50a9d0ec6bb3c3
ahmad-aghapour <aghapour@risklab.ai>
2024-02-25 16:14:09 +0330

    debug completed

--- RiskLabAI/__init__.py
+++ RiskLabAI/__init__.py
@@ -2,12 +2,11 @@
 from . import data
 from . import backtest
 from . import cluster
-from . import cluster
 from . import controller
 
 from . import ensemble
 from . import features
 from . import hpc
-#from . import modeling
+from . import modeling
 from . import pde
 from . import utils
